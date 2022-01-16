31dae1dcfc14e5ba25554beec251c20fa185302c
EpicLulz1509 <duttaarunopal@gmail.com>
2022-01-16 21:56:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pyperclip
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
-dir = "C:\\Users\\KIIT\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\discord"
+dir = "C:\\Users\\User_Name\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\discord"
+#replace dir with path of the discord exe or shortcut file
 os.startfile(dir)
 os.system(dir)
 
@@ -13,6 +14,7 @@ time.sleep(5)
 
 #print(pyautogui.size())    
 print(pyautogui.position())
+#for your own keep your mouse at the text box and then run the program. The position should be printed. Change it in the next line.
 pyautogui.click(x=891, y=1028) 
 
 time.sleep(5)
